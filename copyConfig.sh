7255c891f3b5e9dc6c4ac0977290bdc43fa4a938
Aint <pinevichv@gmail.com>
2022-10-31 19:41:22 +0300

    nginx config
    added https support

---
+++ copyConfig.sh
@@ -0,0 +1,3 @@
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp default /etc/nginx/sites-enabled
+sudo /etc/init.d/nginx reload
--- server/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-httpProxy = require('http-proxy');
-
-httpProxy.createProxyServer({target:'http://89.208.198.137:8081/'}).listen(80);
-
-httpProxy.createProxyServer({target:'http://89.208.198.137:8081/'}).listen(443);
