ea443c11d3731f3d04e017563f89e8beb0096895
Aint <pinevichv@gmail.com>
2022-12-15 17:35:30 +0300

    finally working

--- copyConfig.sh
+++ copyConfig.sh
@@ -1,5 +1,3 @@
 sudo cp default /etc/nginx/conf.d/reazon.conf
-# sudo cp default /etc/nginx/sites-enabled/reazon.conf
-# sudo mv default /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx reload
 systemctl status nginx
