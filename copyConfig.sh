aa8475606be56b60af06a7f9f614ba04df207f85
Aint <pinevichv@gmail.com>
2022-12-15 17:15:56 +0300

    404 fix?

--- copyConfig.sh
+++ copyConfig.sh
@@ -1,3 +1,4 @@
-sudo cp default /etc/nginx/sites-enabled/default
+sudo mv default /etc/nginx/sites-enabled/reazon.conf
+# sudo mv default /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx reload
 systemctl status nginx
